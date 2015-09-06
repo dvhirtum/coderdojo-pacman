4a776f15f098ffde2a66d74f78ea846dfe9f6686
Dick van Hirtum <dvhirtum@gmail.com>
2015-09-06 17:02:01 +0200

    turned boundingbox and gameobject into closures

--- js/boundingbox.js
+++ js/boundingbox.js
@@ -1,6 +1,10 @@
-function BoundingBox (x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-}
+var BoundingBox = (function () {
+  function BoundingBox (x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  return BoundingBox;
+}());
--- js/gameobject.js
+++ js/gameobject.js
@@ -1,26 +1,45 @@
-function GameObject (options) {
-  if (options === undefined || options === null) {
-    options = {};
-  }
-  this.context = options.context;
-  this.image = options.image;
-  this.x = options.x || 0;
-  this.y = options.y || 0;
-
-  this.boundingBoxes = [];
-}
-
-GameObject.prototype.checkCollision = function (otherObject) {
-  if (!(otherObject instanceof GameObject)) {
-    return false;
-  }
-  for (var i = 0; i < this.boundingBoxes.length; i++) {
-    var boundingBox = this.boundingBoxes[i];
-    if (!(boundingBox instanceof BoundingBox)) {
-      continue;
+var GameObject = (function () {
+  function GameObject (options) {
+    if (options === undefined || options === null) {
+      options = {};
     }
-    for (var j = 0; j < otherObject.boundingBoxes.length; j++) {
-      var otherBox = otherObject.boundingBoxes[j];
+    this.context = options.context;
+    this.image = options.image;
+    this.x = options.x || 0;
+    this.y = options.y || 0;
+
+    this.boundingBoxes = [];
+  }
+
+  GameObject.prototype.draw = function () {
+    for (var i = 0; i < this.boundingBoxes.length; i++) {
+      var box = this.boundingBoxes[i];
+      this.context.fillStyle = "magenta";
+      this.context.fillRect(this.x + box.x, this.y + box.y, box.width, box.height);
+    }
+  };
+
+  GameObject.prototype.checkCollision = function (otherObject) {
+    if (!(otherObject instanceof GameObject)) {
+      return false;
+    }
+
+    for (var i = 0; i < this.boundingBoxes.length; i++) {
+      var boundingBox = this.boundingBoxes[i];
+      if (!(boundingBox instanceof BoundingBox)) {
+        continue;
+      }
+      if (checkBoxCollision.call(this, boundingBox, otherObject)) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  function checkBoxCollision (boundingBox, otherObject) {
+    for (var i = 0; i < otherObject.boundingBoxes.length; i++) {
+      var otherBox = otherObject.boundingBoxes[i];
       if (!(otherBox instanceof BoundingBox)) {
         continue;
       }
@@ -39,14 +58,9 @@ GameObject.prototype.checkCollision = function (otherObject) {
 
       return true;
     }
-  }
-  return false;
-};
 
-GameObject.prototype.draw = function () {
-  for (var i = 0; i < this.boundingBoxes.length; i++) {
-    var box = this.boundingBoxes[i];
-    this.context.fillStyle = "magenta";
-    this.context.fillRect(this.x + box.x, this.y + box.y, box.width, box.height);
+    return false;
   }
-};
+
+  return GameObject;
+}());
