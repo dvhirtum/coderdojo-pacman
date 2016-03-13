c87a75a5e52a35d1ff2283f3048954eb55414731
Dick van Hirtum <dvhirtum@gmail.com>
2016-03-13 21:01:32 +0100

    Refactored pacman

--- js/objects/pacman.js
+++ js/objects/pacman.js
@@ -2,42 +2,42 @@ var PacMan = (function () {
   PacMan.prototype = new GameObject();
   PacMan.prototype.constructor = PacMan;
 
-  var animations = {
-    idle: [
-      {x: 0, y: 20}
-    ],
-    left: [
-      {x: 0, y: 20},
-      {x: 96, y: 20},
-      {x: 128, y: 20},
-      {x: 96, y: 20}
-    ],
-    right: [
-      {x: 0, y: 20},
-      {x: 32, y: 20},
-      {x: 64, y: 20},
-      {x: 32, y: 20}
-    ],
-    up: [
-      {x: 0, y: 20},
-      {x: 224, y: 20},
-      {x: 256, y: 20},
-      {x: 224, y: 20}
-    ],
-    down: [
-      {x: 0, y: 20},
-      {x: 160, y: 20},
-      {x: 192, y: 20},
-      {x: 160, y: 20}
-    ]
-  };
-
-  var currentDirection = "";
-  var activeAnimation = animations.idle;
-  var activeAnimationState = 0;
-
   function PacMan (options) {
     GameObject.call(this, options);
+
+    this.animations = {
+      idle: [
+        {x: 0, y: 20}
+      ],
+      left: [
+        {x: 0, y: 20},
+        {x: 96, y: 20},
+        {x: 128, y: 20},
+        {x: 96, y: 20}
+      ],
+      right: [
+        {x: 0, y: 20},
+        {x: 32, y: 20},
+        {x: 64, y: 20},
+        {x: 32, y: 20}
+      ],
+      up: [
+        {x: 0, y: 20},
+        {x: 224, y: 20},
+        {x: 256, y: 20},
+        {x: 224, y: 20}
+      ],
+      down: [
+        {x: 0, y: 20},
+        {x: 160, y: 20},
+        {x: 192, y: 20},
+        {x: 160, y: 20}
+      ]
+    };
+
+    this.currentDirection = "";
+    this.activeAnimation = this.animations.idle;
+    this.activeAnimationState = 0;
 
     this.level = options.level;
 
@@ -52,8 +52,8 @@ var PacMan = (function () {
   PacMan.prototype.draw = function () {
     this.context.drawImage(
       this.image,
-      activeAnimation[activeAnimationState].x,
-      activeAnimation[activeAnimationState].y,
+      this.activeAnimation[this.activeAnimationState].x,
+      this.activeAnimation[this.activeAnimationState].y,
       this.size,
       this.size,
       this.position.x,
@@ -67,21 +67,21 @@ var PacMan = (function () {
       updateSingleStep.call(this, direction);
     }
 
-    if (currentDirection === "left") {
-      activeAnimation = animations.left;
-    } else if (currentDirection === "right") {
-      activeAnimation = animations.right;
-    } else if (currentDirection === "up") {
-      activeAnimation = animations.up;
-    } else if (currentDirection === "down") {
-      activeAnimation = animations.down;
+    if (this.currentDirection === "left") {
+      this.activeAnimation = this.animations.left;
+    } else if (this.currentDirection === "right") {
+      this.activeAnimation = this.animations.right;
+    } else if (this.currentDirection === "up") {
+      this.activeAnimation = this.animations.up;
+    } else if (this.currentDirection === "down") {
+      this.activeAnimation = this.animations.down;
     } else {
-      activeAnimation = animations.idle;
+      this.activeAnimation = this.animations.idle;
     }
 
-    activeAnimationState++;
-    if (activeAnimationState >= activeAnimation.length) {
-      activeAnimationState = 0;
+    this.activeAnimationState++;
+    if (this.activeAnimationState >= this.activeAnimation.length) {
+      this.activeAnimationState = 0;
     }
   };
 
@@ -93,19 +93,19 @@ var PacMan = (function () {
       this.position.x = oldPosition.x;
       this.position.y = oldPosition.y;
 
-      if (currentDirection === direction) {
-        currentDirection = "";
+      if (this.currentDirection === direction) {
+        this.currentDirection = "";
       } else {
-        updatePosition.call(this, currentDirection);
+        updatePosition.call(this, this.currentDirection);
 
         if (hasCollidedWithWall.call(this)) {
           this.position.x = oldPosition.x;
           this.position.y = oldPosition.y;
-          currentDirection = "";
+          this.currentDirection = "";
         }
       }
     } else {
-      currentDirection = direction;
+      this.currentDirection = direction;
     }
   }
 
